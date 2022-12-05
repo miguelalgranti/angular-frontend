cccfd481d40d9a1b8889867068a97d76f7fa5d7c
miguelalgranti <108410813+miguelalgranti@users.noreply.github.com>
2022-12-05 10:53:29 -0300

    v1.8

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { IniciarSesionComponent } from './componentes/iniciar-sesion/iniciar-sesion.component';
 import { PortfolioComponent } from './componentes/portfolio/portfolio.component';
+import { GuardGuard } from './servicios/guard.guard';
 
 const routes: Routes = [
-  {path: 'portfolio', component:PortfolioComponent},
+  {path: 'portfolio', component:PortfolioComponent }, // canActivate:[GuardGuard]
   {path: 'iniciar-sesion', component:IniciarSesionComponent},
   {path: '',redirectTo: 'iniciar-sesion', pathMatch: 'full'}
 ];
