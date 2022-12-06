6a4e4c1a565ef0c73b7af7a9ee1eea48fe43a4e3
miguelalgranti <108410813+miguelalgranti@users.noreply.github.com>
2022-12-06 15:55:06 -0300

    casi todo implementado con jwt pero no levanta

--- src/app/componentes/experiencia/experiencia.component.ts
+++ src/app/componentes/experiencia/experiencia.component.ts
@@ -1,8 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { PortfolioService } from 'src/app/servicios/portfolio.service';
 import { faCoffee } from '@fortawesome/free-solid-svg-icons'; 
 import { FormsModule } from '@angular/forms';  
 import { Observable  } from 'rxjs' 
+import { PortfolioService } from 'src/app/servicios/portfolio.service';
 @Component({
   selector: 'app-experiencia',
   templateUrl: './experiencia.component.html',
