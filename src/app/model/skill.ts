5ff82faef283a6f6b06c6f6ec6bb9c5caf7efb35
miguelalgranti <108410813+miguelalgranti@users.noreply.github.com>
2022-12-29 09:16:01 -0300

    corregido aptitudes

--- src/app/model/skill.ts
+++ src/app/model/skill.ts
@@ -2,10 +2,12 @@ export class Skill {
     id: number;
     nombre: string;
     porcentaje: number;
+    color: string;
 
-    constructor(nombre:string, porcentaje:number){
+    constructor(nombre:string, porcentaje:number, color: string){
         this.nombre = nombre;
         this.porcentaje = porcentaje;
+        this.color = color;
         
     }
 }
