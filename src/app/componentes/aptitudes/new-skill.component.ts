468f1974aec4c925782c946aac11d2f9cd33d336
miguelalgranti <108410813+miguelalgranti@users.noreply.github.com>
2022-12-29 09:20:41 -0300

    ya

--- src/app/componentes/aptitudes/new-skill.component.ts
+++ src/app/componentes/aptitudes/new-skill.component.ts
@@ -11,6 +11,7 @@ import { SkillService } from 'src/app/service/skill.service';
 export class NewSkillComponent implements OnInit {
 nombre: string;
 porcentaje: number;
+color: string;
 
   constructor(private skillS: SkillService, private router: Router) { }
 
@@ -18,7 +19,7 @@ porcentaje: number;
   }
 
   onCreate(): void {
-    const skill = new Skill(this.nombre, this.porcentaje);
+    const skill = new Skill(this.nombre, this.porcentaje, this.color);
     this.skillS.save(skill).subscribe(
       data => {
         alert("Skill creada correctamente");
